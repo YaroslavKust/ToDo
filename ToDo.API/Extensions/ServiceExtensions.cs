4defd4fee17d1f8eada8e13ddc382283c297ee7f
YaroslavKust <79914649+YaroslavKust@users.noreply.github.com>
2021-10-29 21:30:53 +0300

    add swagger

--- ToDo.API/Extensions/ServiceExtensions.cs
+++ ToDo.API/Extensions/ServiceExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
 using ToDo.DataAccess;
 
 namespace ToDo.API.Extensions
@@ -11,6 +12,16 @@ namespace ToDo.API.Extensions
         {
             services.AddDbContext<ToDoContext>(options =>
                 options.UseSqlServer(configuration.GetConnectionString("sqlConnection")));
+        }
+
+        public static void ConfigureSwagger(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(options => options.SwaggerDoc("v1", new OpenApiInfo()
+            {
+                Version = "v1",
+                Title = "ToDo list API",
+                Description = "API for current application"
+            }));
         }
     }
 }
