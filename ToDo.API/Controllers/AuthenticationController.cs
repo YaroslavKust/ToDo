e9679373e1c31c84d1805cc24a72b2a8779bae84
YaroslavKust <79914649+YaroslavKust@users.noreply.github.com>
2021-11-01 00:26:16 +0300

    update controllers

--- ToDo.API/Controllers/AuthenticationController.cs
+++ ToDo.API/Controllers/AuthenticationController.cs
@@ -1,9 +1,9 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
+using Serilog;
+using ToDo.API.ActionFilters;
+using ToDo.API.Authentication;
+using ToDo.Entities.DTO;
 
 namespace ToDo.Web.Controllers
 {
@@ -11,5 +11,43 @@ namespace ToDo.Web.Controllers
     [ApiController]
     public class AuthenticationController : ControllerBase
     {
+        private IAuthenticationManager _authManager;
+        private ILogger _logger;
+
+        public AuthenticationController(IAuthenticationManager authManager, ILogger logger)
+        {
+            _authManager = authManager;
+            _logger = logger;
+        }
+
+
+        [HttpPost("login")]
+        [ServiceFilter(typeof(ValidateModelAttribute))]
+        public async Task<IActionResult> LoginAsync([FromBody] UserForAuth user)
+        {
+            if (await _authManager.ValidateUser(user))
+            {
+                return Ok(new { Token = _authManager.GenerateToken() });
+            }
+
+            _logger.Information("Authorization failed");
+
+            return Unauthorized("Authorization failed, try again");
+        }
+
+
+        [HttpPost]
+        [ServiceFilter(typeof(ValidateModelAttribute))]
+        public async Task<IActionResult> RegisterAsync([FromBody] UserForAuth user)
+        {
+            if (await _authManager.CreateUser(user))
+            {
+                return StatusCode(201);
+            }
+
+            _logger.Information("Cannot register user");
+
+            return BadRequest("Cannot register user");
+        }
     }
 }
