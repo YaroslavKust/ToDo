10686ce3bd420ca606d883a8dab98394e3e8f69c
YaroslavKust <79914649+YaroslavKust@users.noreply.github.com>
2021-11-08 15:36:21 +0300

    add viewmodels for pages

---
+++ MVC/Models/DetailsViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using ToDo.Entities.DTO;
+
+namespace MVC.Models
+{
+    public class DetailsViewModel
+    {
+        public EmployeeDto Employee { get; set; }
+        public IEnumerable<TaskDto> Tasks { get; set; }
+    }
+}
